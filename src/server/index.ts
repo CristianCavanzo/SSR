ae2d567c7306bdbe4f32dadd81a2288930ff9229
Cristian Cavanzo <cristian.frontendev@gmail.com>
2023-08-07 14:58:22 -0500

    [Add]: client side

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,1 +1,13 @@
-console.log('Hola mundo desde el server.');
+import { config } from './config';
+import express, { Express, Request, Response } from 'express';
+import { render } from './render';
+
+const app: Express = express();
+
+app.get('*', (req: Request, res: Response) => {
+    res.send(render(req.url));
+});
+
+app.listen(config.PORT, () => {
+    console.log(`listening in http://localhost:${config.PORT}`);
+});
